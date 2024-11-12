4dcde439e5d7014703f50abde2e637dc6666749b
Jordan <jordan20022@gmail.com>
2024-11-12 15:33:45 +1300

    added more CORS

--- src/config/express.ts
+++ src/config/express.ts
@@ -14,7 +14,7 @@ export default () => {
     app.use(bodyParser.raw({type: ['image/*'], limit: '5mb'}));
     app.use((req, res, next) => {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-Authorization');
         res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
         next();
     });
